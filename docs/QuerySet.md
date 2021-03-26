68acc64a9bd1db841c0913ea4324875365ddc003
zhengtong0898 <zhengtong0898@aliyun.com>
2021-03-26 11:02:26 +0800

    增加 db.models.query.QuerySet.get 的使用案例和源码分析说明.

--- docs/QuerySet.md
+++ docs/QuerySet.md
@@ -1,4 +1,8 @@
 ### get
-[db.models.query.QuerySet.get](../src/Django-3.0.8/django/db/models/query.py#L399)   
-该方法用于获取一条数据.   
-当查询到的结果是多条时, 会报错.
+`db.models.query.QuerySet.get`   
+该方法用于获取一条数据, 当查询到的结果是多条时会抛异常.    
+
+- [使用案例](../orm-examples/myqueryset/get_/tests.py#L8)     
+
+- [源码分析](../src/Django-3.0.8/django/db/models/query.py#L399)
+
--- orm-examples/myqueryset/get_/tests.py
+++ orm-examples/myqueryset/get_/tests.py
@@ -8,11 +8,48 @@ class SimpleTest(TestCase):
     def test_get(self):
         from .models import product
 
-        p = product(name="aaa", price=10.00, description="aaa", production_date="1999-12-31", expiration_date=170)
-        p.save()
+        # 准备数据
+        product(name="aaa", price=10.00, description="aaa", production_date="1999-12-31", expiration_date=170).save()
+        product(name="bbb", price=10.00, description="bbb", production_date="1999-12-31", expiration_date=170).save()
 
-        p = product(name="bbb", price=10.00, description="bbb", production_date="1999-12-31", expiration_date=170)
-        p.save()
+        # 断言-1
+        # SELECT `get__product`.`id`,
+        #        `get__product`.`name`,
+        #        `get__product`.`price`,
+        #        `get__product`.`description`,
+        #        `get__product`.`production_date`,
+        #        `get__product`.`expiration_date`,
+        #        `get__product`.`date_joined`
+        # FROM `get__product`
+        # WHERE `get__product`.`id` = 2
+        # LIMIT 21
+        ss = product.objects.get(pk=1)
+        self.assertEqual(ss.name, "aaa")
 
+        # 断言-2
+        # SELECT `get__product`.`id`,
+        #        `get__product`.`name`,
+        #        `get__product`.`price`,
+        #        `get__product`.`description`,
+        #        `get__product`.`production_date`,
+        #        `get__product`.`expiration_date`,
+        #        `get__product`.`date_joined`
+        # FROM `get__product`
+        # WHERE `get__product`.`id` = 2
+        # LIMIT 21
+        ss = product.objects.get(pk=2)
+        self.assertEqual(ss.name, "bbb")
+
+        # 断言-3:
         with self.assertRaises(Exception):
+            # SELECT `get__product`.`id`,
+            #        `get__product`.`name`,
+            #        `get__product`.`price`,
+            #        `get__product`.`description`,
+            #        `get__product`.`production_date`,
+            #        `get__product`.`expiration_date`,
+            #        `get__product`.`date_joined`
+            # FROM `get__product`
+            # WHERE `get__product`.`expiration_date` = 170
+            # LIMIT 21
             ss = product.objects.get(expiration_date=170)
