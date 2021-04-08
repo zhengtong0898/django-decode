07eb3d397ed7d22536fed1eb9279875ab0779734
zhengtong0898 <zhengtong0898@aliyun.com>
2021-04-09 00:41:58 +0800

    增加 orm子查询 写法

--- orm-examples/nowcoder/SQL1/tests.py
+++ orm-examples/nowcoder/SQL1/tests.py
@@ -2,6 +2,8 @@ from datetime import date
 from django.db import connections
 from django.test import TestCase, TransactionTestCase
 from .models import employees
+from django.db.models import Max
+from django.db.models.expressions import Subquery, OuterRef
 
 
 # Create your tests here.
@@ -9,7 +11,7 @@ class SimpleTest(TransactionTestCase):
 
     reset_sequences = True
 
-    def test_sql_1(self):
+    def prepare_data(self):
         # 建表语句
         # CREATE TABLE `sql1_employees` (
         #   `emp_no` int(11) NOT NULL,
@@ -21,7 +23,6 @@ class SimpleTest(TransactionTestCase):
         #   PRIMARY KEY (`emp_no`)                          # 聚集索引
         # ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-        # 准备数据
         # 一次只能插入一条数据,
         # 如果想要插入多条数据, 需要采用 executemany 配合 insert into sql1_employees values (xxx), (xxx), (xxx);
         cursor = connections['default'].cursor()
@@ -37,6 +38,24 @@ class SimpleTest(TransactionTestCase):
         cursor.execute("""INSERT INTO sql1_employees VALUES(10010,'1963-06-01','Duangkaew','Piveteau','F','1989-08-24');""")
         cursor.execute("""INSERT INTO sql1_employees VALUES(10011,'1953-11-07','Mary','Sluis','F','1990-01-22');""")
 
+    def clear_data(self):
+        cursor = connections['default'].cursor()
+        cursor.execute('delete from sql1_employees;')
+
+    def pre_assert(self, qs):
+        # 断言-期望: 10008|1958-02-19|Saniya|Kalloufi|M|1994-09-15
+        self.assertEqual(len(qs), 1)
+        self.assertEqual(qs[0].emp_no, 10008)
+        self.assertEqual(qs[0].birth_date, date(year=1958, month=2, day=19))
+        self.assertEqual(qs[0].first_name, "Saniya")
+        self.assertEqual(qs[0].last_name, "Kalloufi")
+        self.assertEqual(qs[0].gender, "M")
+        self.assertEqual(qs[0].hire_date, date(year=1994, month=9, day=15))
+
+    def test_sql_1_1(self):
+        # 准备数据
+        self.prepare_data()
+
         # 查找最晚入职员工的所有信息
         # SELECT `SQL1_employees`.`emp_no`,
         #        `SQL1_employees`.`birth_date`,
@@ -49,11 +68,32 @@ class SimpleTest(TransactionTestCase):
         # DESC LIMIT 1
         qs = employees.objects.order_by('-hire_date')[:1]
 
-        # 断言-期望: 10008|1958-02-19|Saniya|Kalloufi|M|1994-09-15
-        self.assertEqual(len(qs), 1)
-        self.assertEqual(qs[0].emp_no, 10008)
-        self.assertEqual(qs[0].birth_date, date(year=1958, month=2, day=19))
-        self.assertEqual(qs[0].first_name, "Saniya")
-        self.assertEqual(qs[0].last_name, "Kalloufi")
-        self.assertEqual(qs[0].gender, "M")
-        self.assertEqual(qs[0].hire_date, date(year=1994, month=9, day=15))
+        # 断言
+        self.pre_assert(qs)
+        
+        # 清空
+        self.clear_data()
+
+    def test_sql_1_2(self):
+        # 准备数据
+        self.prepare_data()
+
+        # 期望SQL
+        # SELECT `SQL1_employees`.`emp_no`,
+        #        `SQL1_employees`.`birth_date`,
+        #        `SQL1_employees`.`first_name`,
+        #        `SQL1_employees`.`last_name`,
+        #        `SQL1_employees`.`gender`,
+        #        `SQL1_employees`.`hire_date`
+        # FROM `SQL1_employees`
+        # WHERE `SQL1_employees`.`hire_date` = (SELECT MAX(U0.`hire_date`) AS `ss`
+        #                                       FROM `SQL1_employees` U0)
+        annotate = employees.objects.annotate(ss=Max('hire_date')).values('ss')
+        annotate.query.group_by = None
+        qs = employees.objects.filter(hire_date=Subquery(annotate))
+
+        # 断言
+        self.pre_assert(qs)
+
+        # 清空
+        self.clear_data()
