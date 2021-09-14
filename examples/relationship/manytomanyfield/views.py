6d86efe921d6e5a18e9d5ee4840f0762a93cc821
zhengtong0898 <zhengtong0898@aliyun.com>
2021-09-15 07:56:47 +0800

    Many-to-many 下

--- examples/relationship/manytomanyfield/views.py
+++ examples/relationship/manytomanyfield/views.py
@@ -20,10 +20,23 @@ def single_create(request):
     a1.publications.add(p1)
 
     # 测试用例-2(反向操作)
-    # 1.
+    # 1. 创建主表数据
+    # INSERT INTO `manytomanyfield_article` (`headline`)
+    # VALUES ('NASA uses Python');
+    aa1 = Article.objects.create(headline='NASA uses Python')
+    # INSERT INTO `manytomanyfield_publication` (`title`)
+    # VALUES ('The Python Journal');
+    pp1 = Publication.objects.create(title='The Python Journal')
+    # 3. 从维表对象的角度触发去执行关联操作, 也被称为反向操作.
+    # INSERT IGNORE INTO `manytomanyfield_article_publications`
+    # (`article_id`, `publication_id`) VALUES (8, 7)               # 这里跟上面的 a1.publications.add(p1)，生成的sql语句一致
+    pp1.article_set.add(aa1)
+
+    # 备注:
+    # 当使用 aa1.publications.all() 时, 是以 aa1 的角度去拉取所有隶属于 aa1 的 publication.
+    # 当使用 pp1.article_set.all() 是, 是以 pp1 的角度去拉取所有从属于 aa1 的 Article.
     return HttpResponse("many_to_many: single_create")
 
 
 def multi_create(request):
-
     return HttpResponse("many_to_many: multi_create")
